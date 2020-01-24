5e6801f0515a4c4892000d857966c09fd9796199
Filippo <pinton.filippo@protonmail.com>
2020-01-24 22:52:48 +0100

    Added images to dashboard cards

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-
   cards = [
-    'Manage Products',
-    'Manage Users',
-    'Statistics'
+    {
+      title: 'Manage Products',
+      image: 'https://media.istockphoto.com/vectors/cleaning-products-vector-id504050034?k=6&m=504050034&s=170667a&w=0&h=Z_wB_XrzzDIfTZ77QW5pb0Fbs8Il4aCdyj2H9ud4j5I='
+    },
+    {
+      title: 'Manage Users',
+      image: 'https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/256x256/users_relation.png'
+    },
+    {
+      title: 'Statistics',
+      image: 'https://www.ft.com/__origami/service/image/v2/images/raw/ftcms%3A347ece48-0f69-11e9-a3aa-118c761d2745?source=ig'
+    }
   ];
 }
